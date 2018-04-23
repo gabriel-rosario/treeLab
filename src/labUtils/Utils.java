10f711c7972a307973f6b06f0864fa09e20e73c2
ICOM STUDENTS <4035b13@pajuil.ece.uprm.edu>
2018-04-23 12:29:11 -0400

    exercise 2

--- src/labUtils/Utils.java
+++ src/labUtils/Utils.java
@@ -42,6 +42,19 @@ public class Utils {
 	public static LinkedBinaryTree<Integer> buildExampleTreeAsLinkedBinaryTree() { 
 		LinkedBinaryTree<Integer> t = new LinkedBinaryTree<>(); 
 		// ADD CODE AS SPECIFIED IN EXERCISE 2
+		t.addRoot(4);
+		Position<Integer> root = t.root();
+		Position<Integer> rL = t.addLeft(t.root(), 9);
+		t.addLeft(rL, 7);
+		t.addRight(rL, 10);
+		Position<Integer> rR = t.addRight(t.root(), 20);
+		Position<Integer> tL = t.addLeft(rR, 15);
+		t.addLeft(tL, 12);
+		t.addLeft(t.addRight(tL, 17), 19);
+		Position<Integer> tR = t.addRight(rR, 21);
+		Position<Integer> t1L = t.addLeft(tR, 40);
+		t.addLeft(t1L, 30);
+		t.addRight(t1L, 45);
 		
 		
 		return t; 
