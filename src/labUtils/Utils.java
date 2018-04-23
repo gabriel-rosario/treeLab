a1c196fdd965f39e9aa03eae75c24b06fde10666
ICOM STUDENTS <4035b13@pajuil.ece.uprm.edu>
2018-04-23 12:20:50 -0400

    exercise 1

--- src/labUtils/Utils.java
+++ src/labUtils/Utils.java
@@ -21,8 +21,21 @@ public class Utils {
 	public static LinkedTree<Integer> buildExampleTreeAsLinkedTree() { 
 		LinkedTree<Integer> t = new LinkedTree<>(); 
 		// ADD CODE AS SPECIFIED IN EXERCISE 2
-		
-		
+		t.addRoot(4);
+		Position<Integer> nine = t.addChild(t.root(), 9);
+		t.addChild(nine, 7);
+		t.addChild(nine,10);
+		Position<Integer> twenty = t.addChild(t.root(), 20);
+		Position<Integer> twentyL = t.addChild(twenty, 15);
+		Position<Integer> twentyR = t.addChild(twenty, 21);
+		Position<Integer> fifteenL = t.addChild(twentyL, 12);
+		Position<Integer> fifteenR = t.addChild(twentyL, 17);
+		t.addChild(fifteenR, 19);
+		Position<Integer> fourty = t.addChild(twentyR, 40);
+		t.addChild(fourty, 30);
+		t.addChild(fourty, 45);
+
+
 		return t; 
 	}
 	
